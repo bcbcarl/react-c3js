73327c697c8cb61c0b7eb57c0e39be08ab8be143
Carl X. Su <bcbcarl@gmail.com>
2016-01-06 02:08:45 +0800

    Update example code

--- example/src/app.js
+++ example/src/app.js
@@ -4,34 +4,61 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import C3Chart from '../../src/index';
 
-class DataLineChart extends React.Component {
-  render() {
+class ExampleChart extends React.Component {
 
-    const data = {
-      url: this.props.src,
-      names: this.props.names,
-      mimeType: 'json'
+  constructor() {
+
+    super();
+
+    this.state = {
+      data: {
+        columns: []
+      }
+    };
+  }
+
+  componentDidMount() {
+
+    const url = 'c3_test.json';
+
+    const names = {
+      data1: 'Name 1',
+      data2: 'Name 2',
+      data3: 'Name 3'
     };
 
+    const mimeType = 'json';
+
+    this.setState({
+      data: {
+        url,
+        names,
+        mimeType
+      }
+    });
+
+    setTimeout(() => this.setState({
+      data: {
+        columns: [
+          ['data4', 30, 200, 100, 400, 150, 250],
+          ['data5', 50, 20, 10, 40, 15, 25]
+        ],
+        names: {
+          data4: 'Name 4',
+          data5: 'Name 5'
+        }
+      }
+    }), 1200);
+  }
+
+  render() {
+
     return (
-      <C3Chart data={data} />
+      <C3Chart data={this.state.data} />
     );
   }
 }
 
-const src = 'c3_test.json';
-
-const names = {
-  data1: 'Name 1',
-  data2: 'Name 2',
-  data3: 'Name 3'
-};
-
 const mountNode = document.getElementById('react-c3js');
 
-ReactDOM.render((
-  <DataLineChart
-    src={src}
-    names={names}
-  />
-), mountNode);
+ReactDOM.render(<ExampleChart />, mountNode);
